e27a0a01770f54eb752591ad6f6c0cdec94a07c3
Anderson Guerra <Andersonguerra61@hotmail.com>
2015-09-22 11:49:44 -0600

    Added Module and View of Departments and Courses

--- EntityDatabase/MainWindow.xaml.vb
+++ EntityDatabase/MainWindow.xaml.vb
@@ -1,10 +1,18 @@
-﻿Class MainWindow
+﻿Imports Modules.Courses.ViewModels
+Imports Modules.Departments.ViewModels
+Class MainWindow
     Sub New()
 
         ' This call is required by the designer.
         InitializeComponent()
 
         ' Add any initialization after the InitializeComponent() call.
+
+        'Departments
+        'Me.DepartamentsUserControl.MainGrid.DataContext = New DepartmentsViewModel
+
+        'Courses
+        Me.CoursesUserControl.MainGrid.DataContext = New CoursesViewModel
 
     End Sub
 End Class
